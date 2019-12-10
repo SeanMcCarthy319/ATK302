86c5ac0337192a9fa28e3a21262b9531afd5bff3
Sean <scmccarthy319@gmail.com>
2019-12-10 03:29:15 -0600

    1

--- p5/Week_6_Daily/sketch.js
+++ p5/Week_6_Daily/sketch.js
@@ -14,24 +14,48 @@ function draw() {
     case 0:
     background('red');
     text("knock, knock", 100, 100);
+    if (myTimer >= 300) {
+
+        myState++ ;  // this will go to the next state. You can also put myState = 3 or something
+        myTimer = 0 ;  // people always forget to reset the timer!!
+
+     }
 
     break;
 
     case 1:
     background('blue');
     text("Who's there?", 100, 100);
+    if (myTimer >= 300) {
+
+        myState++ ;  // this will go to the next state. You can also put myState = 3 or something
+        myTimer = 0 ;  // people always forget to reset the timer!!
+
+     }
 
     break;
 
     case 2:
     background('green');
     text("No one", 100, 100);
+    if (myTimer >= 300) {
+
+        myState++ ;  // this will go to the next state. You can also put myState = 3 or something
+        myTimer = 0 ;  // people always forget to reset the timer!!
+
+     }
 
     break;
 
     case 3:
     background('yellow');
     text("....", 100, 100);
+    if (myTimer >= 300) {
+
+        myState++ ;  // this will go to the next state. You can also put myState = 3 or something
+        myTimer = 0 ;  // people always forget to reset the timer!!
+
+     }
 
     break;
 
